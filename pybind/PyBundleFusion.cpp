6f45c2ff8440f49508a9b7f905f6b472143df57c
Hyunwoo Ryu <tomato1mule@gmail.com>
2023-08-25 16:10:16 +0000

    pybind working

--- pybind/PyBundleFusion.cpp
+++ pybind/PyBundleFusion.cpp
@@ -46,6 +46,16 @@ public:
         // cv::imshow ( "depth_image", depthImage );
         char c = cv::waitKey ( 20 );
         
+
+        // ------------------------------------------------------------------- //
+        // For debugging
+        // ------------------------------------------------------------------- //
+        // for (int i = 0; i < depthImage.rows / 100; i++) {
+        //     for (int j = 0; j < depthImage.cols / 100; j++) {
+        //         std::cout << depthImage.at<float>(i, j) << " ";  // replace float with the type of your matrix, could be uchar, double, etc.
+        //     }
+        //     std::cout << std::endl;
+        // }
         return processInputRGBDFrame(rgbImage, depthImage);
     }
 
@@ -60,12 +70,12 @@ public:
 PYBIND11_MODULE(PyBundleFusion, m) {
     py::class_<BundleFusionWrapper>(m, "BundleFusion")
         .def(py::init<const std::string&, const std::string&>())
-        .def("process", [](BundleFusionWrapper &self, py::array_t<unsigned char> rgbArray, py::array_t<unsigned char> depthArray) {
+        .def("process", [](BundleFusionWrapper &self, py::array_t<unsigned char> rgbArray, py::array_t<uint16_t> depthArray) {
             py::buffer_info rgbInfo = rgbArray.request();
             py::buffer_info depthInfo = depthArray.request();
 
             cv::Mat rgbImage(rgbInfo.shape[0], rgbInfo.shape[1], CV_8UC3, (unsigned char*)rgbInfo.ptr);
-            cv::Mat depthImage(depthInfo.shape[0], depthInfo.shape[1], CV_8UC1, (unsigned char*)depthInfo.ptr);
+            cv::Mat depthImage(depthInfo.shape[0], depthInfo.shape[1], CV_16UC1, (uint16_t*)depthInfo.ptr);
 
             return self.process(rgbImage, depthImage);
         })
